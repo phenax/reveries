ac3dd578072bf565aa2377f153d9e60f5f9e6012
Akshay Nair <phenax5@gmail.com>
2025-12-26 20:24:58 +0530

    Refactor clock allocation

--- dashboard/src/clock.zig
+++ dashboard/src/clock.zig
@@ -7,57 +7,47 @@ pub const ClockWidget = struct {
         return ClockWidget{};
     }
 
-    pub fn draw(_: ClockWidget, alloc: std.mem.Allocator) error{OutOfMemory}!void {
-        var timestr: [8:0]u8 = undefined;
-        var datestr: [64:0]u8 = undefined;
+    pub fn draw(_: ClockWidget, _: std.mem.Allocator) !void {
+        const width = @divFloor(rl.getScreenWidth() * 5, 9);
+        const timeFontSize = 170;
+        const dateFontSize = 40;
 
         // TODO: Remove timezone hardcoding
         const zone = dt.timezones.Asia.Kolkata;
-
         var now = dt.datetime.Datetime.now();
         now = now.shiftSeconds(zone.offsetSeconds());
 
-        const timefmt = try formatTime(now, alloc);
-        defer alloc.free(timefmt);
-        timestr[timefmt.len] = 0;
-        std.mem.copyForwards(u8, &timestr, timefmt);
-
-        const datefmt = try formatDate(now, alloc);
-        defer alloc.free(datefmt);
-        datestr[datefmt.len] = 0;
-        std.mem.copyForwards(u8, &datestr, datefmt);
-
-        const width = @divFloor(rl.getScreenWidth() * 5, 9);
-
-        const timeFontSize = 140;
-        const dateFontSize = 40;
-
-        const timeTextWidth = rl.measureText(&timestr, timeFontSize);
+        // Time
+        var timefmtbuf: [8:0]u8 = undefined;
+        const timefmt = try formatTime(now, &timefmtbuf);
+        const timeTextWidth = rl.measureText(timefmt, timeFontSize);
         const timex = @divFloor(width - timeTextWidth, 2);
-        const y = 16;
-        rl.drawText(&timestr, timex, y, timeFontSize, .white);
+        const timey = 16;
+        rl.drawText(timefmt, timex, timey, timeFontSize, .white);
 
-        const dateTextWidth = rl.measureText(&datestr, dateFontSize);
+        // Date
+        var datefmtbuf: [64:0]u8 = undefined;
+        const datefmt = try formatDate(now, &datefmtbuf);
+        const dateTextWidth = rl.measureText(datefmt, dateFontSize);
         const datex = @divFloor(width - dateTextWidth, 2);
-        rl.drawText(&datestr, datex, 180, dateFontSize, .white);
+        const datey = 170;
+        rl.drawText(datefmt, datex, datey, dateFontSize, .white);
     }
 };
 
-pub fn formatTime(time: dt.datetime.Datetime, alloc: std.mem.Allocator) ![]u8 {
+pub fn formatTime(time: dt.datetime.Datetime, buf: []u8) ![:0]const u8 {
     const hour = if (time.time.hour > 12) time.time.hour - 12 else time.time.hour;
-    const timefmt = try std.fmt.allocPrint(
-        alloc,
+    return try std.fmt.bufPrintZ(
+        buf,
         "{d:0>2}:{d:0>2}",
         .{ hour, time.time.minute },
     );
-    return timefmt;
 }
 
-pub fn formatDate(time: dt.datetime.Datetime, alloc: std.mem.Allocator) ![]u8 {
-    const timefmt = try std.fmt.allocPrint(
-        alloc,
+pub fn formatDate(time: dt.datetime.Datetime, buf: []u8) ![:0]const u8 {
+    return try std.fmt.bufPrintZ(
+        buf,
         "{s}, {d} {s}",
         .{ time.date.weekdayName(), time.date.day, time.date.monthName() },
     );
-    return timefmt;
 }
