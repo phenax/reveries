4b85231c545f399653f00eb2d5fe1ed0b2590855
Akshay Nair <phenax5@gmail.com>
2025-12-27 15:02:49 +0530

    Handles image cropping for slideshow

--- dashboard/src/slideshow.zig
+++ dashboard/src/slideshow.zig
@@ -2,12 +2,16 @@ const std = @import("std");
 const rl = @import("raylib");
 const dt = @import("datetime");
 
+const defaultSlideDurationSeconds: u16 = 30;
+
 pub const SlideshowWidget = struct {
     texturesCache: std.StringHashMap(rl.Texture),
     filePaths: std.ArrayList([]u8),
-    current: usize = 0,
+    current: usize = 1,
     nextSlideTimestamp: i64,
-    slideDuration: u16,
+    slideDurationSeconds: u16 = defaultSlideDurationSeconds,
+    paddingX: i32 = 0,
+    paddingY: i32 = 0,
 
     pub fn new(alloc: std.mem.Allocator) !SlideshowWidget {
         var localArena = std.heap.ArenaAllocator.init(std.heap.c_allocator);
@@ -18,14 +22,13 @@ pub const SlideshowWidget = struct {
         const homeDir = envMap.get("HOME") orelse "/";
         const appDir = try std.fmt.allocPrint(localAlloc, "{s}/Pictures/slideshow", .{homeDir});
         std.debug.print("[log] albumdir: {s}\n", .{appDir});
-
         const albumDir = std.fs.openDirAbsolute(appDir, .{ .iterate = true }) catch mkdir: {
             try std.fs.makeDirAbsolute(appDir);
             break :mkdir try std.fs.openDirAbsolute(appDir, .{ .iterate = true });
         };
+
         var dirIter = albumDir.iterate();
         var filePaths: std.ArrayList([]u8) = .empty;
-
         while (try dirIter.next()) |file| {
             if (file.kind == .file) {
                 const path = try albumDir.realpathAlloc(alloc, file.name);
@@ -34,12 +37,12 @@ pub const SlideshowWidget = struct {
             }
         }
 
-        const slideDuration = 2;
+        const slideDurationSeconds = defaultSlideDurationSeconds;
         return SlideshowWidget{
             .filePaths = filePaths,
             .texturesCache = std.StringHashMap(rl.Texture).init(alloc),
-            .nextSlideTimestamp = std.time.timestamp() + slideDuration,
-            .slideDuration = slideDuration,
+            .nextSlideTimestamp = std.time.timestamp() + slideDurationSeconds,
+            .slideDurationSeconds = slideDurationSeconds,
         };
     }
 
@@ -51,8 +54,8 @@ pub const SlideshowWidget = struct {
         if (slideshow.current > count) return;
 
         if (try slideshow.getTexture()) |texture| {
-            const imgx = width;
-            const imgy = 20;
+            const imgx = width + slideshow.paddingX;
+            const imgy = slideshow.paddingY;
             rl.drawTexture(texture, imgx, imgy, .white);
         }
 
@@ -60,15 +63,8 @@ pub const SlideshowWidget = struct {
         const now = std.time.timestamp();
         if (slideshow.nextSlideTimestamp < now) {
             slideshow.current = @mod(slideshow.current + 1, count);
-            slideshow.nextSlideTimestamp = now + slideshow.slideDuration;
-            std.debug.print(":::: {d} {d}\n", .{ slideshow.current, count });
+            slideshow.nextSlideTimestamp = now + slideshow.slideDurationSeconds;
         }
-    }
-
-    fn loadImageIntoCache(slideshow: *SlideshowWidget, path: []u8) !rl.Texture {
-        const texture = try loadImageTexture(path);
-        try slideshow.texturesCache.put(path, texture);
-        return texture;
     }
 
     fn getTexture(slideshow: *SlideshowWidget) !?rl.Texture {
@@ -77,8 +73,44 @@ pub const SlideshowWidget = struct {
         if (slideshow.current > count) return null;
 
         const path = slideshow.filePaths.items[slideshow.current];
-        std.debug.print("- loading {s}\n", .{path});
         return slideshow.texturesCache.get(path) orelse try slideshow.loadImageIntoCache(path);
+    }
+
+    fn loadImageIntoCache(slideshow: *SlideshowWidget, path: []u8) !rl.Texture {
+        const texture = try slideshow.loadImageTexture(path);
+        std.debug.print("- loading {s}\n", .{path});
+        try slideshow.texturesCache.put(path, texture);
+        return texture;
+    }
+
+    fn loadImageTexture(slideshow: SlideshowWidget, path: []u8) !rl.Texture {
+        var pathbuf: [std.fs.max_path_bytes:0]u8 = undefined;
+        std.mem.copyForwards(u8, &pathbuf, path);
+        pathbuf[path.len] = 0;
+        var img = try rl.loadImage(&pathbuf);
+        defer rl.unloadImage(img);
+
+        const sw = @divFloor(rl.getScreenWidth() * 4, 9) - 2 * slideshow.paddingX;
+        const sh = rl.getScreenHeight() - 2 * slideshow.paddingY;
+        const boxAspectRatio = @as(f32, @floatFromInt(sw)) / @as(f32, @floatFromInt(sh));
+
+        std.debug.print(":::::: fooo {d}x{d}\n", .{ sw, sh });
+
+        const imgHeight = @as(f32, @floatFromInt(img.height));
+        const imgWidth = @as(f32, @floatFromInt(img.width));
+        // const aspectRatio = imgWidth / imgHeight;
+
+        const isLandscape = img.width > img.height;
+        const www = if (isLandscape) imgHeight * boxAspectRatio else imgWidth;
+        const hhh = if (isLandscape) imgHeight else imgWidth / boxAspectRatio;
+        const offsetX = (imgWidth - www) / 2;
+        const offsetY = (imgHeight - hhh) / 2;
+        std.debug.print(":::::: swsh {d}x{d}:{d}-{d}\n", .{ offsetX, offsetY, imgWidth - 2 * offsetX, imgHeight - 2 * offsetY });
+
+        rl.imageCrop(&img, rl.Rectangle.init(@floor(offsetX), @floor(offsetY), @floor(imgWidth - 2 * offsetX), @floor(imgHeight - 2 * offsetY)));
+        rl.imageResize(&img, sw, sh);
+
+        return try rl.loadTextureFromImage(img);
     }
 
     pub fn deinit(slideshow: *SlideshowWidget, alloc: std.mem.Allocator) !void {
@@ -89,21 +121,3 @@ pub const SlideshowWidget = struct {
         slideshow.texturesCache.deinit();
     }
 };
-
-fn loadImageTexture(path: []u8) !rl.Texture {
-    var pathbuf: [std.fs.max_path_bytes:0]u8 = undefined;
-    std.mem.copyForwards(u8, &pathbuf, path);
-    pathbuf[path.len] = 0;
-    var img = try rl.loadImage(&pathbuf);
-    defer rl.unloadImage(img);
-
-    const padding = 40;
-    rl.imageCrop(&img, rl.Rectangle.init(0, @floatFromInt(padding), @floatFromInt(img.width), @floatFromInt(img.height - padding)));
-    const aspectRatio = @as(f32, @floatFromInt(img.width)) / @as(f32, @floatFromInt(img.height));
-    const height = rl.getScreenHeight(); // 320
-    const width = @as(i32, @intFromFloat(@as(f32, @floatFromInt(height)) * aspectRatio));
-    rl.imageResize(&img, width, height - 40);
-    const texture = try rl.loadTextureFromImage(img);
-
-    return texture;
-}
